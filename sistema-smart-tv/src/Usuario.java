62d72a178e963efba05ec1df62e9d472f5647f1a
Jálisson.Silva <jalissondasilva@gmail.com>
2024-06-04 09:31:09 -0300

    Unificação de repositórios

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,34 @@
+
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+    
+        smartTV smartTV = new smartTV();
+
+        System.out.println("TV Ligada ? " + smartTV.ligada);        
+        System.out.println("Canal Atual : " +" "+ smartTV.canal);        
+        System.out.println("Volume Atual : " +" "+ smartTV.volume);  
+        
+        smartTV.ligar ();
+        System.out.println("Novo status - TV Ligada ? " + smartTV.ligada);   
+
+        smartTV.desligar();
+        System.out.println("Novo status - TV Ligada ? " + smartTV.ligada);
+        
+        
+        System.out.println("Canal Atual : " +" "+ smartTV.canal);   
+
+        smartTV.diminuirVolume(); 
+        smartTV.diminuirVolume();
+        smartTV.diminuirVolume();
+        smartTV.diminuirVolume();
+        smartTV.diminuirVolume();
+    
+        System.out.println("Volume Atual : " +" "+ smartTV.volume); 
+
+        System.out.println("Canal Atual : " +" "+ smartTV.canal); 
+        smartTV.mudarCanal(13);
+        System.out.println("Canal Atual : " +" "+ smartTV.canal); 
+
+    }
+    
+}
