3b486fe9d900d0420eeb0d1a87b6711ba843452c
Jálisson.Silva <jalissondasilva@gmail.com>
2024-06-05 10:06:23 -0300

    Novos capítulos estudados

---
+++ Controle-Fluxo/src/TestesEEntendimento.java
@@ -0,0 +1,12 @@
+public class TestesEEntendimento {
+    public static void main(String[] args) {
+        String[] nomes = {"Camila", "Venilton", "Leonardo", "Renan", "Rafael"};
+        System.out.print(nomes.length);
+
+        String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+
+	for (int x=0; x<alunos.length; x++) {
+		System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
+	}
+    }
+}
